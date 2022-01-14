19c57c06511f3cef79056d352ff02a6d3af8b8b8
YuyaIida <i12.yuya.i15@gmail.com>
2022-01-14 10:16:09 +0900

    fix: add @versiong

--- tcpServier.ts
+++ tcpServier.ts
@@ -1,4 +1,4 @@
-import { copy } from "https://deno.land/std/streams/conversion.ts";
+import { copy } from "https://deno.land/std@0.121.0/streams/conversion.ts";
 
 const hostname = "0.0.0.0";
 const port = 8080;
